708a9fe6d36a4a7b9b7fac41a379cfea6cc38568
juliandamass <juliandamass@gmail.com>
2022-12-14 21:29:07 +0700

    Update

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
-const { ServerConfig } = require("./src/configs/ServerConfig");
-
-const backendUrl = ServerConfig.getPublicBackendUrl();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${backendUrl}/:path*`,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
